1ec76e55889772ff632bb581bb3b998c9962f2ed
Iuliia <mbeq9665>
2017-12-03 18:11:28 +0300

    add img

---
+++ src/Music.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Music extends React.Component {
+    render() {
+        return <h1>{this.props.name}</h1>
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import logo from './logo.svg';
 import './App.css';
+import Music from './Music';
 
 let rootElement = (
     <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-
-        </header>
+        <div className="body">
+            <body>
+                <Music name="Радио тапок"/>
+            </body>
+        </div>
     </div>
 );
 
