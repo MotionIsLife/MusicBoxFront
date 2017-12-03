6b2720ccdd71a5dec38b27135cc033b74a5e65db
Iuliia <mbeq9665>
2017-12-03 15:53:17 +0300

    Небольшой рефакторинг

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import logo from './logo.svg';
+import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let rootElement = (
+    <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+
+        </header>
+    </div>
+);
+
+ReactDOM.render(rootElement, document.getElementById('root'));
