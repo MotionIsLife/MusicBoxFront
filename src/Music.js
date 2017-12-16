31c02a9a409bdb70b79b815b6476f398f1c0ae3d
Iuliia <mbeq9665>
2017-12-16 21:23:21 +0300

    Добавление redux

--- src/Music.js
+++ src/Music.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { RaisedButton, TextField } from 'material-ui';
 
 export default class Music extends React.Component {
     render() {
-        return <h1>{this.props.name}</h1>
+        return (
+            <div>
+        <h1>{this.props.name}</h1>
+                <TextField
+                    defaultValue="Default Value"
+                    floatingLabelText="Floating Label Text"
+                />
+                <RaisedButton label="Primary" primary={true} style={{margin: 12}} />
+            </div>
+    )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import './App.css';
 import Music from './Music';
+
+//фун меняющая стор
+//2. узнает о выстреле
+//3. меняет данные в сторе
+function playList(state = [], action){
+    if(action.type === 'ADD_TRACK'){
+//возвращаем новую копию стора
+        return [
+            //добавление в стор
+            ...state,
+            action.payload
+        ]
+    }
+    return state;
+}
+
+//создаем новый стор для приложения
+const store = createStore(playList);
+// начальное состояние стора
+console.log(store.getState());
+
+//подписались на изменение стора с помощью функции subscribe
+store.subscribe(() => {
+    console.log('subscribe', store.getState());
+});
+
+//action-ы с типами и данными
+//1. стреляет
+store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like spirit' });
+store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
 
 let rootElement = (
     <div className="App">
         <div className="body">
             <body>
                 <Music name="Радио тапок"/>
-            </body>
+            </body>`
         </div>
     </div>
 );
